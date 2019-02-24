746ae4494d2ee0d8e719f0caced76db4cad8c96e
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-02-24 01:00:30 -0800

    Add isUnique.

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ package com.raghvendra.ctci;
 public class Main {
 
     public static void main(String[] args) {
+        //isUnique
+        String str = "hello";
 	    isUnique iu = new isUnique();
+        System.out.println("Output: "+ iu.isUnique(str));
     }
 }
--- isUnique.java
+++ isUnique.java
@@ -1,5 +1,16 @@
 package com.raghvendra.ctci;
 
 public class isUnique {
-
+    public boolean isUnique(String s){
+        char[] wordMap = new char[255];
+        for (char c : s.toCharArray()){
+            wordMap[c]++;
+        }
+        for(int i : wordMap){
+            if(i > 1){
+                return false;
+            }
+        }
+        return true;
+    }
 }
